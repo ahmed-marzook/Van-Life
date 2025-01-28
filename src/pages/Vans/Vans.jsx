ae03bc1685c26959285d7a4c61a2d0ef151547c9
Ahmed Marzook <m4marzook@gmail.com>
2025-01-28 22:21:41 +0000

    Made layout more responsive

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -8,10 +8,10 @@ export default function Vans() {
         <h1 className="vans__heading">Explore our van options</h1>
       </section>
       <section className="vans_options">
-        <button className="vans__options-buttons">Simple</button>
-        <button className="vans__options-buttons">Luxury</button>
-        <button className="vans__options-buttons">Rugged</button>
-        <button className="vans__options-clear-button">Clear filters</button>
+        <button className="vans__options-button">Simple</button>
+        <button className="vans__options-button">Luxury</button>
+        <button className="vans__options-button">Rugged</button>
+        <button className="vans__options-button clear">Clear filters</button>
       </section>
       <section className="vans__list">
         {/* van one */}
