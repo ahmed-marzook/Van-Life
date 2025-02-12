951aaa38be507686f803c9f870c86f1f64195a3f
Ahmed Marzook <m4marzook@gmail.com>
2025-02-12 17:20:19 +0000

    Ability to porccess error and disable button when login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,52 +1,42 @@
-import { useState } from "react";
 import "./Login.css";
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
+import { loginUser } from "../../api/loginUser";
 
 export function loader({ request }) {
   return new URL(request.url).searchParams.get("message");
 }
 
 export default function Login() {
-  const [loginFormData, setLoginFormData] = useState({
-    email: "",
-    password: "",
-  });
-
+  const [status, setStatus] = useState("idle");
+  const [error, setError] = useState(null);
   const message = useLoaderData();
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    console.log(loginFormData);
-  }
-
-  function handleChange(e) {
-    const { name, value } = e.target;
-    setLoginFormData((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
+  function submitForm(formData) {
+    setStatus("submitting");
+    loginUser({
+      email: formData.get("email"),
+      password: formData.get("password"),
+    })
+      .then((data) => {
+        console.log(data);
+        setError(null);
+      })
+      .catch((err) => setError(err))
+      .finally(() => setStatus("idle"));
   }
 
   return (
     <div className="login-container">
       {message && <h2>{message}</h2>}
+      {error && <h3>{error.message}</h3>}
       <h1>Sign in to your account</h1>
-      <form onSubmit={handleSubmit} className="login-form">
-        <input
-          name="email"
-          onChange={handleChange}
-          type="email"
-          placeholder="Email address"
-          value={loginFormData.email}
-        />
-        <input
-          name="password"
-          onChange={handleChange}
-          type="password"
-          placeholder="Password"
-          value={loginFormData.password}
-        />
-        <button>Log in</button>
+      <form action={submitForm} className="login-form">
+        <input name="email" type="email" placeholder="Email address" />
+        <input name="password" type="password" placeholder="Password" />
+        <button type="submit" disabled={status === "submitting"}>
+          {status === "submitting" ? "Logging in..." : "Log in"}
+        </button>
       </form>
     </div>
   );
